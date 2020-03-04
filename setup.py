2310a8bd3b75df0cf816643aba8444672a297058
Einar Huseby <einar.huseby@gmail.com>
2020-03-04 12:56:52 +0100

    Removed json dep

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nif-tools",
+    version="0.7.4",
+    author="Einar Huseby",
+    author_email="einar.huseby@gmail.com",
+    description="Tools to programmatically interact with NIF's Min Idrett, Klubbadmin and Sportsadmin",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luftsport/nif-tools",
+    packages=setuptools.find_packages(),
+    install_requires=['requests', 'dateutil', 'bs4'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
